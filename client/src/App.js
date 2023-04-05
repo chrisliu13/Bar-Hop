ba4480276889aa1ed1283e59ee2a0697fb3eac61
Christopher Liu <43731113+chrisliu13@users.noreply.github.com>
2023-04-04 21:03:57 -0500

    connected client to server... back to front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
 import React, {useEffect, useState} from 'react'
 import {Footer, Blog, Possibility, Features, WhatGPT3, Header} from './containers'
-import {CTA, Brand, Navbar} from './components'
+import {Button, CTA, Brand, Navbar} from './components'
 const App = () => {
   return(
     <div className = "App">
       <div className = "gradient__bg">
-        <Navbar />
-        <Header />
+        
       </div>
-      <Brand/>
-      <WhatGPT3/>
-      <Features/>
-      <Possibility/>
-      <CTA/>
-      <Blog/>
-      <Footer/>
     </div>
   )
 }
--- server/server.js
+++ server/server.js
@@ -1,8 +1,23 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 
-app.get("/api", (req,res) =>{
-    res.json({"user": ["userOne", "userTwo", "userThree"]})
-})
+// create a variable to store the number of votes
+let voteCount = 0;
 
-app.listen(5000, ()=>{console.log("Server started on port 5000")})
+// parse application/json
+app.use(bodyParser.json());
+
+// create an endpoint to get the vote count
+app.get('/api', (req, res) => {
+    res.json({ votes: voteCount });
+});
+
+// create an endpoint to handle the vote button click
+app.post('/api', (req, res) => {
+  // increment the vote count
+  voteCount++;
+  res.json({ votes: voteCount });
+});
+
+app.listen(5000, () => console.log(`Server started on port 5000`));
