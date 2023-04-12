20b29dba43f6b2be22422a7edfd4cca88b34782f
Christopher Liu <43731113+chrisliu13@users.noreply.github.com>
2023-04-11 23:33:36 -0500

    working on nav bar

--- client/src/App.js
+++ client/src/App.js
@@ -6,8 +6,16 @@ const App = () => {
   return(
     <div className = "App">
       <div className = "gradient__bg">
-        
+        <Navbar/>
+        <Header/>
       </div>
+      <Brand/>
+      <WhatGPT3/>
+      <Features/>
+      <Possibility/>
+      <CTA/>
+      <Blog/>
+      <Footer/>
     </div>
   )
 }
--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
-import './navbar.css'
+import React from 'react';
+import './navbar.css';
+import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import logo from '../../assets/logo.svg'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className = 'barhop__navbar'>
+      <div className = 'barhop__navbar-links'>
+        <div className = 'barhop__navbar-links-logo'>
+          <img src = {logo} alt = "logo"/>
+        </div>
+        <div className = "barhop__navbar-links_container">
+          <p><a href = "#home"> Home</a></p>
+          <p><a href = "#functions"> Functionality</a></p>
+          <p><a href = "#start"> Getting Started</a></p>
+          <p><a href = "#charts"> Top Charts</a></p>
+          <p><a href = "#vote"> Vote</a></p>
+        </div>
+      </div>
+    </div>
   )
 }
 
