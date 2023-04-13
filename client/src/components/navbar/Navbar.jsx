b359ce52c9aea5daedc3d1b0df702bcb0402ce3c
Christopher Liu <43731113+chrisliu13@users.noreply.github.com>
2023-04-12 19:39:12 -0500

    done with navbar

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './navbar.css';
-import {RiMenu3Line, RiCloseLin} from 'react-icons/ri';
+import {RiMenu3Line, RiCloseLine} from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
+const Menu = () =>(
+  <>
+    <p><a href = "#home"> Home</a></p>
+    <p><a href = "#functions"> Functionality</a></p>
+    <p><a href = "#start"> Getting Started</a></p>
+    <p><a href = "#charts"> Top Charts</a></p>
+    <p><a href = "#vote"> Vote</a></p>
+  </>
+)
 const Navbar = () => {
+  //toggleMenu = true if showing mobile menu, false otherwise
+  //setToggleMenu = function that can change the variable
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <div className = 'barhop__navbar'>
       <div className = 'barhop__navbar-links'>
@@ -10,12 +22,22 @@ const Navbar = () => {
           <img src = {logo} alt = "logo"/>
         </div>
         <div className = "barhop__navbar-links_container">
-          <p><a href = "#home"> Home</a></p>
-          <p><a href = "#functions"> Functionality</a></p>
-          <p><a href = "#start"> Getting Started</a></p>
-          <p><a href = "#charts"> Top Charts</a></p>
-          <p><a href = "#vote"> Vote</a></p>
+          <Menu/>
         </div>
+      </div>
+      <div className = "barhop__navbar-menu">
+        {toggleMenu
+        ? <RiCloseLine color = "#fff" size = {27} onClick = {() => setToggleMenu(false)}/>
+        :<RiMenu3Line color = "#fff" size = {27} onClick = {() => setToggleMenu(true)}/>
+        }
+        {toggleMenu && (
+          <div className = "barhop__navbar-menu_container scale-up-center">
+            <div className = "barhop__navbar-menu_container-links">
+              <Menu/>
+            </div>
+          </div>
+        )
+        }
       </div>
     </div>
   )
