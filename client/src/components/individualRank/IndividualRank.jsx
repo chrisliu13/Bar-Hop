71cb1f66e1f1660a6c0e4373133289d07bb60c07
Christopher Liu <43731113+chrisliu13@users.noreply.github.com>
2023-04-22 12:11:43 -0500

    created rankings folder and classes

---
+++ client/src/components/individualRank/IndividualRank.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './individualrank.css'
+//this class represents each individual bar that will be 
+//assesed in the overall "ranking" component
+const IndividualRank = () => {
+  return (
+    <div>IndividualRank</div>
+  )
+}
+
+export default IndividualRank
--- client/src/containers/topvote/Topvote.jsx
+++ client/src/containers/topvote/Topvote.jsx
@@ -1,11 +1,16 @@
 import './topvote.css';
 import React from 'react';
 import {Vote} from '../../components';
+import {Ranking} from '../../containers';
+//this class stores the bar vote section along with the rankings
 const Topvote = () => {
   return (
     <div className = "bh__topvote section_margin" id = "charts">
       <div className = "bh__topvote-vote">
-        <Vote/>
+        <Vote/> {/*shows vote option */}
+      </div>
+      <div>
+        <Ranking/> {/*shows US news ranking */}
       </div>
     </div>
   )
